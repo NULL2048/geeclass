7c51ce729cd0a3150eaa19487733ca13a0f6a6e8
NULL2048 <973071263@qq.com>
2021-02-05 16:08:42 +0800

    添加Example，自动生成sql

---
+++ server/src/main/java/pers/cy/geeclass/server/service/TestService.java
@@ -0,0 +1,26 @@
+package pers.cy.geeclass.server.service;
+
+import pers.cy.geeclass.server.domain.Test;
+import pers.cy.geeclass.server.domain.TestExample;
+import pers.cy.geeclass.server.mapper.TestMapper;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        TestExample testExample = new TestExample();
+
+        // 相当于一个where条件  下面这个表示查找id字段为1的数据
+        testExample.createCriteria().andIdEqualTo("1");
+
+        // 设置排序
+        testExample.setOrderByClause("id asc");
+
+        return testMapper.selectByExample(testExample);
+    }
+}
