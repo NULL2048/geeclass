dac74c959c7975641c09a123802ecf88af792164
NULL2048 <973071263@qq.com>
2021-04-09 22:43:52 +0800

    解决gateway跨域问题

--- gateway/src/main/java/pers/cy/geeclass/gateway/GatewayApplication.java
+++ gateway/src/main/java/pers/cy/geeclass/gateway/GatewayApplication.java
@@ -5,7 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,6 +27,31 @@ public class GatewayApplication {
 		Environment env = app.run(args).getEnvironment();
 		LOG.info("启动成功！！");
 		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+	/**
+	 * 配置跨域
+	 * 因为使用了路由，所以跨域配置就不要写到业务模块server了，直接在网关配置就可以了
+	 * 一般用spring cloud就在网关配置跨域，如果是spring boot单体项目，那么直接在业务模块添加跨域配置类（CorsConfig）就可以了
+	 * @return
+	 */
+	@Bean
+	public CorsWebFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(Boolean.TRUE);
+		config.addAllowedMethod("*");
+
+		// 需要用addAllowedOriginPattern方法
+		config.addAllowedOriginPattern("*");
+
+		config.addAllowedHeader("*");
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsWebFilter(source);
 	}
 
 }
--- server/src/main/java/pers/cy/geeclass/server/config/CorsConfig.java
+++ server/src/main/java/pers/cy/geeclass/server/config/CorsConfig.java
@@ -1,25 +1,28 @@
-package pers.cy.geeclass.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        /**
-         * allowCredentials为true时，allowedOrigins不能包含特殊值“*”
-         * 因为不能在“Access Control Allow Origin”响应头上设置该值。
-         * 要允许凭据访问一组源，请显式列出它们，或者考虑改用“allowedOriginPatterns”
-         */
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
-    }
-}
+//package pers.cy.geeclass.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        /**
+//         * allowCredentials为true时，allowedOrigins不能包含特殊值“*”
+//         * 因为不能在“Access Control Allow Origin”响应头上设置该值。
+//         * 要允许凭据访问一组源，请显式列出它们，或者考虑改用“allowedOriginPatterns”
+//         *
+//         * 上面这个错误是将spring boot升级为2.4.0而导致的
+//         * https://blog.csdn.net/ASAS1314/article/details/110524116
+//         */
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+//    }
+//}
