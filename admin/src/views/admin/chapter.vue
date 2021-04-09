a422d3a255aa9cc12f024d1fbd470725e4336303
NULL2048 <973071263@qq.com>
2021-04-09 21:35:16 +0800

    使用gateway路由转发，vue页面只能访问gateway的端口

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -10,66 +10,6 @@
         </thead>
 
         <tbody>
-        <tr>
-          <td>123</td>
-          <td>test</td>
-          <td>111</td>
-
-          <td>
-            <div class="hidden-sm hidden-xs btn-group">
-              <button class="btn btn-xs btn-success">
-                <i class="ace-icon fa fa-check bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-info">
-                <i class="ace-icon fa fa-pencil bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-danger">
-                <i class="ace-icon fa fa-trash-o bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-warning">
-                <i class="ace-icon fa fa-flag bigger-120"></i>
-              </button>
-            </div>
-
-            <div class="hidden-md hidden-lg">
-              <div class="inline pos-rel">
-                <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                  <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                </button>
-
-                <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                  <li>
-                    <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-																			<span class="blue">
-																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
-																			</span>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-																			<span class="green">
-																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-																			</span>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-																			<span class="red">
-																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
-																			</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </td>
-        </tr>
-
         <tr v-for="chapter in chapters">
           <td>{{chapter.id}}</td>
           <td>{{chapter.name}}</td>
@@ -152,7 +92,8 @@ export default {
       let _this = this;
       // 向接口做了一个list请求
       // 这个是从前端localhost访问127.0.0.1的后端访问路径，会出现跨域问题，所以这里需要解决跨域问题
-      _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+      // 这里请求直接到路由网关中，再由网关进行转发
+      _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
         console.log("查询大章列表结果：", response);
         _this.chapters = response.data;
       })
