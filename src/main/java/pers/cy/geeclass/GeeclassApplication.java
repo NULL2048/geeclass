980e73b371c3920eaff2fae13f354dbfed75c8a5
NULL2048 <973071263@qq.com>
2021-04-11 14:13:11 +0800

    回滚——分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解

--- src/main/java/pers/cy/geeclass/GeeclassApplication.java
+++ src/main/java/pers/cy/geeclass/GeeclassApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeeclassApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(GeeclassApplication.class, args);
 	}
+
 }
