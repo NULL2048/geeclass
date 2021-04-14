dd44ea5aadd96fe79159ff422324e19be0e8ba3d
NULL2048 <973071263@qq.com>
2021-04-14 09:48:50 +0800

    1、增加日志AOP，统一日志输出 2、logback增加打印日志跟踪号

--- business/src/main/java/pers/cy/geeclass/business/controller/admin/ChapterController.java
+++ business/src/main/java/pers/cy/geeclass/business/controller/admin/ChapterController.java
@@ -18,6 +18,8 @@ import java.util.List;
 public class ChapterController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+    public static final String BUSINESS_NAME = "大章";
+
     @Resource
     private ChapterService chapterService;
 
