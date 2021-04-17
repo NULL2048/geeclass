3a97581b6f9cb0b465e54919e09cae5d24a56d2b
NULL2048 <973071263@qq.com>
2021-04-17 21:04:26 +0800

    文件上传功能开发：文件上传实时显示与保存

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -88,6 +88,7 @@
                         <label class="col-sm-2 control-label">头像</label>
                         <div class="col-sm-10">
                             <input type="file" v-on:change="uploadImage()" id="file-upload-input">
+                          <img v-bind:src="teacher.image" class="img-responsive">
                         </div>
                     </div>
                     <div class="form-group">
@@ -239,9 +240,10 @@
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
           Loading.hide();
           let resp = response.data;
+          let image = resp.content;
+          _this.teacher.image = image;
         });
-        // let image = resp.content.path;
-        // _this.teacher.image = image;
+
       }
     }
   }
