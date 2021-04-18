507a7c3c628a24ecb8dd96591ccd7c4e4082e508
NULL2048 <973071263@qq.com>
2021-04-18 14:46:12 +0800

    1、文件上传功能开发：制作文件上传公共组件 2、修复不能连续上传同一个文件得BUG

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -87,11 +87,12 @@
                     <div class="form-group">
                         <label class="col-sm-2 control-label">头像</label>
                         <div class="col-sm-10">
-                          <button type="button" v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
-                            <i class="ace-icon fa fa-upload"></i>
-                            上传头像
-                          </button>
-                            <input class="hidden" type="file" ref="file" v-on:change="uploadImage()" id="file-upload-input">
+
+                          <file v-bind:id="'image-upload'"
+                                v-bind:text="'上传头像'"
+                                v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                                v-bind:after-upload="afterUpload"></file>
+
                           <div v-show="teacher.image" class="row">
                             <div class="col-md-4">
                               <img v-bind:src="teacher.image" class="img-responsive">
@@ -131,10 +132,11 @@
 
 <script>
   import Pagination from "../../components/pagination";
+  import File from "../../components/file";
 
   export default {
     name: 'business-teacher',
-    components: {Pagination},
+    components: {Pagination, File},
     data: function () {
       return {
         teacher: {},
@@ -240,42 +242,10 @@
         })
       },
 
-      uploadImage(resp) {
+      afterUpload(resp) {
         let _this = this;
-        let formData = new window.FormData();
-        let file = _this.$refs.file.files[0];
-
-        // 判断文件格式
-        let suffixs = ["jpg", "jpeg", "png"];
-        let fileName = file.name;
-        let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
-        let validateSuffix = false;
-        for (let i = 0; i < suffixs.length; i++) {
-          if (suffixs[i].toLowerCase() === suffix) {
-            validateSuffix = true;
-            break;
-          }
-        }
-        if (!validateSuffix) {
-          Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
-          // $("#" + _this.inputId + "-input").val("");
-          return;
-        }
-
-
-        formData.append('file', file);
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          let image = resp.content;
-          _this.teacher.image = image;
-        });
-
-      },
-
-      selectImage() {
-        $("#file-upload-input").trigger("click");
+        let image = resp.content;
+        _this.teacher.image = image;
       }
     }
   }
