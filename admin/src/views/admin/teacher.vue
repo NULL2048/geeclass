1a84e0998f3c3b1d2c364600182e9867541e8281
NULL2048 <973071263@qq.com>
2021-04-18 16:34:34 +0800

    文件上传功能开发：文件上传成功后返回值的处理

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -245,7 +245,7 @@
 
       afterUpload(resp) {
         let _this = this;
-        let image = resp.content;
+        let image = resp.content.path;
         _this.teacher.image = image;
       }
     }
