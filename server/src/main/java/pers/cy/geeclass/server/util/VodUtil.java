d31c76f420f3f9cd58ef2c9623fa49892554e315
NULL2048 <973071263@qq.com>
2021-04-23 19:43:19 +0800

    视频加密与授权播放：基于OSS原生SDK上传视频到点播服务，视频点播不支持追加上传

--- server/src/main/java/pers/cy/geeclass/server/util/VodUtil.java
+++ server/src/main/java/pers/cy/geeclass/server/util/VodUtil.java
@@ -48,7 +48,7 @@ public class VodUtil {
 //        request.setCoverURL("http://vod.aliyun.com/test_cover_url.jpg");
 //        request.setCateId(1000115308L);
 //        request.setTemplateGroupId("78fffb8c0c2426efd5baaaafed76fe36");
-        
+
         //request.setWorkflowId("");
         //request.setStorageLocation("");
         //request.setAppId("app-1000000");
