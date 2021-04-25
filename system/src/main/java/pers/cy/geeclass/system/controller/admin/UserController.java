d556565d2853cc71ecbb61ac925e63d166fc03ef
NULL2048 <973071263@qq.com>
2021-04-25 17:25:51 +0800

    用户管理与登录：密码加密传输与加密存储

--- system/src/main/java/pers/cy/geeclass/system/controller/admin/UserController.java
+++ system/src/main/java/pers/cy/geeclass/system/controller/admin/UserController.java
@@ -2,6 +2,7 @@ package pers.cy.geeclass.system.controller.admin;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 import pers.cy.geeclass.server.domain.User;
 import pers.cy.geeclass.server.dto.UserDto;
@@ -43,6 +44,7 @@ public class UserController {
      */
     @PostMapping("/save")
     public ResponseDto save(@RequestBody UserDto userDto) {
+        userDto.setPassword(DigestUtils.md5DigestAsHex(userDto.getPassword().getBytes()));
 
         // 保存校验
         ValidatorUtil.require(userDto.getLoginName(), "登录名");
