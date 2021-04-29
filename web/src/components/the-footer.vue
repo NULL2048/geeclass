c407f2b404fe9ef5a20a45d0945ddb28f7fc2283
NULL2048 <973071263@qq.com>
2021-04-29 15:47:09 +0800

    网站首页开发：将底部提取成the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">回到顶部</a>
+      </p>
+      <p>欢迎使用极课网在线教育平台</p>
+      <p>2021&nbsp;geeclass.com&nbsp;&nbsp;冀ICP备 2017234060219号</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+  export default {
+    name: 'theFooter',
+  }
+</script>
