880f5db1675d9a5090cd35320100280f46009c03
NULL2048 <973071263@qq.com>
2021-04-30 15:19:37 +0800

    网站首页开发：增加课程card组件the-course

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -19,25 +19,7 @@
         <div class="title1">最新上线</div>
         <div class="row">
           <div v-for="o in news" v-bind:key="o" class="col-md-4">
-            <div class="card mb-4 shadow-sm course">
-
-              <img class="img-fluid" v-bind:src="o.image">
-              <div class="card-body">
-                <h4 class="">{{o.name}}</h4>
-                <p class="card-text">{{o.summary}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-
-                  <div class="text-muted">
-                    <span class="badge badge-info"> <i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.price}}</span>&nbsp;
-                    <span class="badge badge-info"> <i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
+            <the-course v-bind:course="o"></the-course>
           </div>
 
         </div>
@@ -47,25 +29,7 @@
         <div class="title2">好课推荐</div>
         <div class="row">
           <div v-for="o in news" v-bind:key="o" class="col-md-4">
-            <div class="card mb-4 shadow-sm course">
-
-              <img class="img-fluid" v-bind:src="o.image">
-              <div class="card-body">
-                <h4 class="">{{o.name}}</h4>
-                <p class="card-text">{{o.summary}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-
-                  <div class="text-muted">
-                    <span class="badge badge-info"> <i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.price}}</span>&nbsp;
-                    <span class="badge badge-info"> <i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
+            <the-course v-bind:course="o"></the-course>
           </div>
 
         </div>
@@ -73,50 +37,14 @@
     </div>
 
   </main>
-
-  <!--  <main role="main">-->
-
-<!--    <section class="jumbotron text-center">-->
-<!--      <div class="container">-->
-<!--        <h1>在线视频课程平台</h1>-->
-<!--        <p class="lead text-muted m-3">-->
-<!--          知识付费时代刚刚起步，在这个领域有很多的发展机会。整个课程以实战为基础，手把手从零开始，一步一步搭建一个完整的企业级开发架构。不讲废话，只讲干货。-->
-<!--        </p>-->
-<!--        <p>-->
-<!--          <router-link to="/list" class="btn btn-primary my-2 p-3 font-weight-bold">点击进入所有课程</router-link>-->
-<!--        </p>-->
-<!--      </div>-->
-<!--    </section>-->
-
-<!--    <div class="album py-5 bg-light">-->
-<!--      <div class="container">-->
-<!--        <div class="title1">最新上线</div>-->
-<!--        <div class="row">-->
-<!--          <div v-for="o in news" class="col-md-4">-->
-<!--            <the-course v-bind:course="o"></the-course>-->
-<!--          </div>-->
-<!--        </div>-->
-
-<!--        <hr>-->
-
-<!--        <div class="title2">好课推荐</div>-->
-<!--        <div class="row">-->
-<!--          <div v-for="o in news" class="col-md-4">-->
-<!--            <the-course v-bind:course="o"></the-course>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-
-<!--  </main>-->
 </template>
 
 <script>
 
-  // import TheCourse from "../components/the-course";
+  import TheCourse from "../components/the-course";
   export default {
     name: 'index',
-    // components: {TheCourse},
+    components: {TheCourse},
     data: function () {
       return {
         news: [],
